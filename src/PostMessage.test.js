348accc84cdac7f1e1129fef938bb5c2aa90c9f7
Emil Nordh <me@emilnordh.se>
2018-05-02 13:36:19 +0200

    tests(eventEmitter): add unit tests for post message module

---
+++ src/PostMessage.test.js
@@ -0,0 +1,44 @@
+import PostMessage from "./PostMessage";
+
+describe('PostMessage', () => {
+  let iframe;
+
+  beforeEach(() => {
+    iframe = {
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      contentWindow: {
+        postMessage: jest.fn(),
+      },
+    };
+  });
+
+  it('should load iframe on initiation', () => {
+    const postMessage = new PostMessage(iframe);
+
+    expect(iframe.addEventListener).toHaveBeenCalledWith('load', expect.any(Function));
+  });
+
+  it('should remove iframe listener when loaded', () => {
+    iframe.addEventListener = jest.fn((eventName, callback) => {
+      callback();
+
+      expect(eventName).toBe('load');
+      expect(iframe.removeEventListener).toHaveBeenCalledWith('load', expect.any(Function));
+    });
+
+    new PostMessage(iframe);
+  });
+
+  it('should load iframe before publishing events', () => {
+    const postMessage = new PostMessage(iframe);
+    const event = { eventName: 'some event name', data: 'some event data' };
+    iframe.addEventListener = jest.fn((eventName, callback) => {
+      expect(eventName).toBe('load');
+      callback();
+
+      expect(iframe.contentWindow.postMessage).toHaveBeenCalledWith(event, '*');
+    });
+    postMessage.publish(event.eventName, event.data);
+  });
+});
